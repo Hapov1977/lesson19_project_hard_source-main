14044fdf94ead93b6d94bad28097f91b196a603c
1 <avhapov@gmail.com>
2022-07-15 00:36:24 +0300

    код

---
+++ views/auth.py
@@ -0,0 +1,35 @@
+from flask import request
+
+from flask_restx import Namespace, Resource
+
+from implemented import auth_service
+
+auth_ns = Namespace('auth')
+
+@auth_ns.route("/")
+class AuthView(Resource):
+    def post(self):
+        reg_json = request.json
+        username = reg_json.get("username")
+        password = reg_json.get("password")
+        if not (username or password):
+            return "Нужно имя или пароль", 400
+
+        tokens = auth_service.generate_tokens(username, password)
+        if tokens:
+            return tokens
+        else:
+            return "Ошибка в запросе", 400
+
+    def put(self):
+        reg_json = request.json
+        ref_token = reg_json.get("refresh_token")
+        if not ref_token:
+            return "Не задан токен", 400
+
+        tokens = auth_service.approve_refresh_token(ref_token)
+        if tokens:
+            return tokens
+        else:
+            return "Ошибка в запросе", 400
+
